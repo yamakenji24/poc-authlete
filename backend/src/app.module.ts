093e0b00ffb8a1b8598b30446d7d8bc1a68085e6
yamakenji24 <yamakenji24@gmail.com>
2025-05-11 14:25:58 +0900

    不要になったので消す

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { HttpModule } from '@nestjs/axios';
-import { AuthModule } from './auth/auth.module';
-
-@Module({
-  imports: [ConfigModule.forRoot(), HttpModule, AuthModule],
-})
-export class AppModule {}
